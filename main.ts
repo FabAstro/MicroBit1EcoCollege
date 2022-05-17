8a0dc59f09ca300f0a707f73d5d31e5b89b72f85
FabAstro <105211458+FabAstro@users.noreply.github.com>
2022-05-17 13:37:28 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+let gamestarted = false
+basic.forever(function () {
+    gamestarted = false
+    basic.pause(randint(1000, 5000))
+    gamestarted = true
+    basic.showIcon(IconNames.TShirt)
+    while (gamestarted) {
+        if (input.pinIsPressed(TouchPin.P1)) {
+            gamestarted = 0
+        }
+    }
+    basic.pause(3000)
+    basic.clearScreen()
+})
